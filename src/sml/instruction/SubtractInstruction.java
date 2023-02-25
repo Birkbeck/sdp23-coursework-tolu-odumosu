e52a7c7a5df7e86d047eb3d7dccc689535a38f0b
tolu-odumosu <toluwalopeodumosu@gmail.com>
2023-02-25 11:31:49 +0000

    Updated SubtractInstruction file

--- src/sml/instruction/SubtractInstruction.java
+++ src/sml/instruction/SubtractInstruction.java
@@ -1,4 +1,18 @@
 package sml.instruction;
 
-public class SubtractInstruction {
+import sml.Instruction;
+import sml.Machine;
+import sml.RegisterName;
+public class SubtractInstruction extends Instruction {
+    private final RegisterName result;
+    private final RegisterName source;
+
+    public static final String OP_CODE = "subtract";
+
+    public SubtractInstruction(String label, RegisterName result, RegisterName source) {
+        super(label, OP_CODE);
+        this.result = result;
+        this.source = source;
+    }
 }
+
